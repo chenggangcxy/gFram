ab7f1f3589c354bcb72bed1c18837940e9ad713a
ChenggangCompany <chenggang_cxy@163.com>
2022-05-19 17:36:13 +0800

    go for it

--- controllers/ruirui/userController.go
+++ controllers/ruirui/userController.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 
 //"html/template"
 func Index(c *gin.Context) {
-	c.HTML(200, "/default/index.html", gin.H{
+	c.HTML(200, "default/index.html", gin.H{
 		"title": "首页+1",
 		"score": 80,
 		"name":  "zhangsan",
--- routers/adminRouters.go
+++ routers/adminRouters.go
@@ -1,15 +1,21 @@
 package routers
 
 import (
+	"fmt"
 	"ruirui/controllers/admin"
 
 	"github.com/gin-gonic/gin"
 )
 
+//中间件
+func initMiddleware(c *gin.Context) {
+	fmt.Println("aaa")
+}
+
 func AdminRouters(r *gin.Engine) {
 	adminRouters := r.Group("/admin")
 	{
-		adminRouters.GET("/", admin.UserContoller{}.UserIndex)
+		adminRouters.GET("/", initMiddleware, admin.UserContoller{}.UserIndex)
 		adminRouters.GET("/useradd", admin.UserContoller{}.UserAdd)
 
 	}
