269aa2dd7493ffc71f8c9fca90117c2d1ed95f33
bighanmmer <chenggang_cxy@163.com>
2022-05-28 23:38:54 +0800

    SD

--- models/tools.go
+++ models/tools.go
@@ -5,6 +5,7 @@ import (
 	//"github.com/golang/protobuf/ptypes/timestamp"
 )
 
+//时间戳转化为标准时间
 func UnixToTime(timestamp int) string {
 	t := time.Unix(int64(timestamp), 0)
 	return t.Format("2006-01-02 15:04:05")
